4fb407b8c983d0ee748fb38341f277b16e89305d
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2021-05-15 09:25:23 +1000

    Update CNN-Keras-Tuner.py
    Best results from testing (val_accuracy=0.9915):
    * Conv layers: 5
    * layer 0 filters: 64
    * dense layers: 3
    * Learning rate: 0.0001
    * layer 1 filters: 32
    * layer 2 filters 16
    * layer 3 filters: 64
    * Layer 0 units: 128
    * Learning rate: 0.1
    * Layer 1 units: 128
    * Layer 2 units: 128
    * Layer 3 units: 64
    * Layer 4 filters: 128
    * Layer 4 units: 128

--- CNN-Keras-Tuner.py
+++ CNN-Keras-Tuner.py
@@ -1,4 +1,3 @@
-# Import libraries
 import tensorflow as tf
 import tensorflow.keras.datasets
 from tensorflow import keras
@@ -6,7 +5,7 @@ from keras import layers
 from kerastuner.tuners import RandomSearch
 from tensorflow.keras.datasets import mnist
 
-# Create the data subsets
+
 (train_data, train_labels), (test_data, test_labels) = mnist.load_data()
 
 # Function to handle preprocessing the data
@@ -18,43 +17,35 @@ def preprocess(train_data, test_data):
   test_data = test_data / 255
   print('Test data max: ', test_data.max())
   print('Test data min: ', test_data.min())
-  # Add a dimenstion to the tensor to represent the color channels
-  train_data = tf.expand_dims(train_data, -1)
-  print('Train data shape: ', train_data.shape)
-  test_data = tf.expand_dims(test_data, -1)
-  print('Test data shape: ',test_data.shape)
+
+
+
+train_data = train_data.reshape(-1,28,28,1)
+test_data = test_data.reshape(-1,28,28,1)
   
 # Perform the preprocessing on the training and testing data
 preprocess(train_data=train_data, test_data=test_data)
 
 
+print(train_data.shape)
+print(test_data.shape)
+
 def create_model(hp):
 
   model = tf.keras.Sequential()
 
-  for i in range(hp.Int('Conv layers', min_value=0, max_value=3)):
-    model.add(keras.layers.Conv2D(hp.Choice(f'layers {i} filters', [16, 32, 64, 128]), 3, activation='relu'))
-  
-  model.add(layers.MaxPooling2D(2,2)),
-  model.add(layers.Flatten()),
-  model.add(layers.Dense(units=hp.Int('units',
-                                      min_value=32,
-                                      max_value=128,
-                                      step=32),
-                         activation='relu')),
-  model.add(layers.Dropout(rate=0.3)),
-  model.add(layers.Dense(units=hp.Int('units',
-                                      min_value=32,
-                                      max_value=128,
-                                      step=32),
-                         activation='relu')),
-  model.add(layers.Dropout(rate=0.3)),
-  model.add(layers.Dense(units=hp.Int('units',
-                                      min_value=32,
-                                      max_value=128,
-                                      step=32),
-                         activation='relu')),
-  model.add(layers.Dropout(rate=0.3)),
+  model.add(tf.keras.layers.Conv2D(filters=16, kernel_size=3, activation='relu', input_shape=train_data.shape[1:]))
+
+  for i in range(hp.Int('Conv layers', min_value=1, max_value=5)):
+    model.add(tf.keras.layers.Conv2D(hp.Choice(f'layers {i} filters', [16,32,64,128]), kernel_size=3, activation='relu'))
+
+  model.add(tf.keras.layers.MaxPooling2D(2,2))
+  model.add(tf.keras.layers.Flatten())
+
+  for i in range(hp.Int('Dense layers', min_value=0, max_value=5)):
+    model.add(tf.keras.layers.Dense(hp.Choice(f'layers {i} Units', [16, 32, 64, 128]), activation='relu'))
+    model.add(tf.keras.layers.Dropout(hp.Choice(f'rate', [0.1,0.2,0.3,0.4,0.5])))
+
   model.add(layers.Dense(10, activation='softmax')), 
   model.compile(loss='sparse_categorical_crossentropy',
                 optimizer=tf.keras.optimizers.Adam(hp.Choice('learning rate',
@@ -63,13 +54,12 @@ def create_model(hp):
   return model
 
 
-
 tuner=RandomSearch(
     create_model,
     objective='val_accuracy',
-    max_trials=10,
+    max_trials=100,
     executions_per_trial=1,
-    directory='mnist_tuner_dir',
+    directory='full_dir',
     project_name='mnist_test'
 )
 
