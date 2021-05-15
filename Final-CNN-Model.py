8a4d8af6d35402e2d7c7051eac50235e5410fda5
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2021-05-15 10:23:53 +1000

    Update Final-CNN-Model.py
    Results:
    * Loss: 0.008
    * Accuracy: 0.9974
    * Val loss: 0.0272
    * Val accuracy: 0.9935

--- Final-CNN-Model.py
+++ Final-CNN-Model.py
@@ -1,1 +1,51 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 
+
+(train_data, train_labels), (test_data, test_labels) = tf.keras.datasets.mnist.load_data()
+
+
+def preprocess(train_data, test_data):
+    train_data = train_data / 255
+    print('Train data min: ', train_data.min())
+    print('Train data max: ', train_data.max())
+    test_data = test_data / 255
+    print('Test data min: ', test_data.min())
+    print('test data max: ', test_data.max())
+
+train_data = train_data.reshape(-1, 28, 28, 1)
+test_data = test_data.reshape(-1, 28, 28, 1)
+
+preprocess(train_data=train_data, test_data=test_data)
+print(train_data.shape)
+print(test_data.shape)
+
+
+tf.random.set_seed(42)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(filters=16, kernel_size=3, activation='relu', input_shape=train_data.shape[1:]),
+    tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+    tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'),
+    tf.keras.layers.Conv2D(filters=16, kernel_size=3, activation='relu'),
+    tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
+    tf.keras.layers.Conv2D(filters=128, kernel_size=3, activation='relu'),
+
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Flatten(),
+
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])    
+
+model.compile(loss='sparse_categorical_crossentropy',
+            optimizer=tf.keras.optimizers.Adam(lr=0.0001),
+            metrics=['accuracy'])
+
+model.fit(train_data, train_labels, epochs=10, validation_data=(test_data, test_labels))
