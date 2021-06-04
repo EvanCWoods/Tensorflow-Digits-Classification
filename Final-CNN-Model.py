5a65863ab05943ce43be2575399f5f1f01dc2360
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2021-06-04 21:43:05 +1000

    Update Final-CNN-Model.py
    added a callback function to stop training at 97% accuracy

--- Final-CNN-Model.py
+++ Final-CNN-Model.py
@@ -25,6 +25,14 @@ print(test_data.shape)
 
 tf.random.set_seed(42)
 
+class MyCallback(tf.keras.preprocessing.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('accuracy')>0.97):
+            print('97% accuracy achieved, stopping training')
+            self.model.stop_training = True
+            
+callback = myCallback()
+
 model = tf.keras.Sequential([
     tf.keras.layers.Conv2D(filters=16, kernel_size=3, activation='relu', input_shape=train_data.shape[1:]),
     tf.keras.layers.Conv2D(filters=64, kernel_size=3, activation='relu'),
@@ -48,4 +56,4 @@ model.compile(loss='sparse_categorical_crossentropy',
             optimizer=tf.keras.optimizers.Adam(lr=0.0001),
             metrics=['accuracy'])
 
-model.fit(train_data, train_labels, epochs=10, validation_data=(test_data, test_labels))
+model.fit(train_data, train_labels, epochs=10, validation_data=(test_data, test_labels), callbacks=[callback])
